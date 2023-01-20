d9f759cf0ec3250ffaa3bb31741e585b5b8d0e4d
Petya <petyageorgieva.pg@gmail.com>
2023-01-20 23:20:40 +0200

    Adding current hw to GitHub

---
+++ 01. Basic Syntax, Conditional Statements and Loops Exercise/multiplication.js
@@ -0,0 +1,10 @@
+function multiplication (num) {
+
+    for (let x = 1; x <= 10; x++) {
+        let product = x * num;
+        console.log(`${num} X ${x} = ${product}`);
+    }
+
+}
+
+multiplication(5)

b5f947c69e2ae5a6278689cb49a2ee0e297f7893
Petya <petyageorgieva.pg@gmail.com>
2023-02-11 18:53:48 +0200

    hw

---
+++ 05. Arrays Exercise/dungeonestDark.js
@@ -0,0 +1,5 @@
+function dungeonestDark (arr) {
+    
+}
+
+dungeonestDark(["rat 10|bat 20|potion 10|rat 10|chest 100|boss 70|chest 1000"])
--- 05. Arrays Exercise/maxNum.js
+++ 05. Arrays Exercise/maxNum.js
@@ -1,12 +1,20 @@
 function maxNumber (arr) {
+    let result = " ";
+
     for (let i = 0; i < arr.length; i++) {
         let num = arr[i];
-        let num2 = arr[i + 1];
+        let num2 = i;
 
-        if (num2 > num) {
-            console.log(num2);
+        if (num > arr[++num2] && i < arr.length && num > arr[arr.length - 1]) {
+            result += num + " ";
+        }
+
+        if (i == arr.length - 1) {
+            result += arr[i];
         }
     }
+
+    console.log(result);
 }
 
 maxNumber([1, 4, 3, 2])
